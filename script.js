3fcb994e845e2f09eb914501dbf2c10f6a2f2649
priyatomer <37611817+priyatomer@users.noreply.github.com>
2018-07-31 16:20:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
+var x = "";
+
 function loadCurrencies() {
  
-	let baseCurrency=document.getElementById("basecurrency").value;
+	var baseCurrency=document.querySelector("#baseCurrency").value;
 	
-	let queryURL=https://exchangeratesapi.io/api/latest?base${baseCurrency};
+	let queryURL= `https://exchangeratesapi.io/api/latest?base=${baseCurrency}`;
 	
 	fetch(queryURL)
 		.then(function (response) {
 			return response.json();
-		}}
+		})
 		.then(function (result) {
 			console.log(result);
 			displayCurrencyResult(result);
-		}}
+		})
 		.catch(function (error) {
 			alert("Something went wrong,  check internet connectivity or invalid currency");
 			console.log(error.message);
-		}};
+		});
 }
 
 function displayCurrencyResult(result) {
-	let msgDiv = document.querySelector("#message");
+	let outputDiv = document.querySelector("#output");
 	
+	for (a in result.rates) {
+
+		x += `<div class="jumbotron text-center"> <h1> ` + a + ` = ` + result.rates[a] + `</h1> </div>`;
+
+	}
+ 
+	$('#output').append(x);
+
+}
+
+
+
+
+
+
+
+	/*
 	let div=document.querySelector("#rates");
 	
 	div.innerHTML ="";
 	msgDiv.innerHTML = <div class ="">
+	*/
